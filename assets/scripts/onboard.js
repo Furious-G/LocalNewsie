31fdcf064a586496e7a9f71c33dc3644a6d3249a
Furious <glamias@gmail.com>
2023-08-11 19:15:07 -0700

    Fix mobile menu

--- assets/scripts/onboard.js
+++ assets/scripts/onboard.js
@@ -1,10 +1,9 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
 
-// Check to see if complete after filling out 
+    // Check to see if complete after filling out 
 
-     jQuery(".acc input").on('blur click', function(e) {
-
+    jQuery(".acc input").on('blur click', function (e) {
         var mess = jQuery(this).closest(".acc").find(".wpcf7-not-valid-tip").length;
 
         // If all good then give the complete
@@ -12,18 +11,18 @@ jQuery(document).ready(function() {
 
 
 
-		            // set name
-		            var currentText = jQuery(this).closest(".acc").find(".businessName").val();
-		            // Enter name
-		            jQuery(this).closest(".acc").children("a").children(".bname").text(currentText);
-		            // Add Check
+            // set name
+            var currentText = jQuery(this).closest(".acc").find(".businessName").val();
+            // Enter name
+            jQuery(this).closest(".acc").children("a").children(".bname").text(currentText);
+            // Add Check
 
-		            jQuery(this).closest(".acc").children("a").children(".check").addClass("showit");
+            jQuery(this).closest(".acc").children("a").children(".check").addClass("showit");
 
-		            jQuery(this).closest(".acc").children("a").children(".pending").removeClass("showit");
+            jQuery(this).closest(".acc").children("a").children(".pending").removeClass("showit");
 
 
-		            jQuery(this).closest(".acc").children("a:first-child").removeClass("orangy");
+            jQuery(this).closest(".acc").children("a:first-child").removeClass("orangy");
 
 
         } else {
@@ -45,13 +44,13 @@ jQuery(document).ready(function() {
 
     });
 
-// Check to see if forms are complete on page load
+    // Check to see if forms are complete on page load
 
-    jQuery(".acc").each(function() {
- 
- 
+    jQuery(".acc").each(function () {
+
+
         // If all good then give the complete
-        if ((jQuery(this).find(".businessName").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".webUrl").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".facebookLink").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".instagramName").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".phone").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".email").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).closest(".acc").find("input[type='checkbox']").is(":checked")) ) {
+        if ((jQuery(this).find(".businessName").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".webUrl").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".facebookLink").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".instagramName").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".phone").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".email").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).closest(".acc").find("input[type='checkbox']").is(":checked"))) {
             // set name
             var currentText = jQuery(this).find(".businessName").val();
             // Enter name
@@ -59,7 +58,7 @@ jQuery(document).ready(function() {
             // Add Check
             jQuery(this).find(".check").addClass('showit');
 
-         
+
 
 
 
@@ -78,12 +77,12 @@ jQuery(document).ready(function() {
 
 
     // Update progress bar after filling form
-    jQuery(".acc input").on('blur click', function(e) {
+    jQuery(".acc input").on('blur click', function (e) {
         var busItems = jQuery('.accordion.bus li').length;
         var numItems = jQuery('.accordion.bus .check.showit').length;
-        var multNum = 100/busItems;
+        var multNum = 100 / busItems;
         var totaldone = multNum * numItems;
-            totaldone = Math.round(totaldone);
+        totaldone = Math.round(totaldone);
 
         jQuery(".filled").css("width", totaldone + "%");
         jQuery(".comp").text(totaldone);
@@ -100,13 +99,13 @@ jQuery(document).ready(function() {
 
         }
 
-        if(totaldone <= 25){
-        	jQuery(".filled").addClass('half');
-        } 
+        if (totaldone <= 25) {
+            jQuery(".filled").addClass('half');
+        }
 
-         if(totaldone >= 50){
-        	jQuery(".filled").addClass('quart');
-        } 
+        if (totaldone >= 50) {
+            jQuery(".filled").addClass('quart');
+        }
 
 
     });
@@ -114,11 +113,11 @@ jQuery(document).ready(function() {
 
 
     // Update progress bar on page load
-        var busItems = jQuery('.accordion.bus li').length;
-        var numItems = jQuery('.accordion.bus .check.showit').length;
-        var multNum = 100/busItems;
-        var totaldone = multNum * numItems;
-            totaldone = Math.round(totaldone);
+    var busItems = jQuery('.accordion.bus li').length;
+    var numItems = jQuery('.accordion.bus .check.showit').length;
+    var multNum = 100 / busItems;
+    var totaldone = multNum * numItems;
+    totaldone = Math.round(totaldone);
 
     jQuery(".filled").css("width", totaldone + "%");
     jQuery(".comp").text(totaldone);
@@ -127,31 +126,31 @@ jQuery(document).ready(function() {
     if (totaldone < 100) {
 
         jQuery(".accordion.bus .wpcf7-submit").attr('disabled', 'disabled');
-    } else{
+    } else {
         jQuery(".accordion.bus a.decoy").addClass('off');
         jQuery(".accordion.bus .wpcf7-submit").removeClass('off');
     }
 
-    if(totaldone <= 25){
-    	jQuery(".filled").addClass('half');
-    } 
+    if (totaldone <= 25) {
+        jQuery(".filled").addClass('half');
+    }
 
-    if(totaldone >= 50){
-    	jQuery(".filled").addClass('quart');
-    } 
-
-
-jQuery(".mask").mask("(999) 999-9999");
+    if (totaldone >= 50) {
+        jQuery(".filled").addClass('quart');
+    }
 
 
+    jQuery(".mask").mask("(999) 999-9999");
 
 
 
-// ########################## CHANNEL TABS
+
+
+    // ########################## CHANNEL TABS
 
 
 
-     jQuery(".acc2 input").on('blur click', function(e) {
+    jQuery(".acc2 input").on('blur click', function (e) {
 
         var mess2 = jQuery(this).closest(".acc2").find(".wpcf7-not-valid-tip").length;
 
@@ -161,17 +160,17 @@ jQuery(".mask").mask("(999) 999-9999");
 
 
 
-                    // set name
-                    var currentText = jQuery(this).closest(".acc2").find(".publisherName").val();
-                    // Enter name
-                    jQuery(this).closest(".acc2").children("a").children(".bname").text(currentText);
-                    // Add Check
+            // set name
+            var currentText = jQuery(this).closest(".acc2").find(".publisherName").val();
+            // Enter name
+            jQuery(this).closest(".acc2").children("a").children(".bname").text(currentText);
+            // Add Check
 
-                    jQuery(this).closest(".acc2").children("a").children(".check").addClass("showit");
+            jQuery(this).closest(".acc2").children("a").children(".check").addClass("showit");
 
-                    jQuery(this).closest(".acc2").children("a").children(".pending").removeClass("showit");
+            jQuery(this).closest(".acc2").children("a").children(".pending").removeClass("showit");
 
-                    jQuery(this).closest(".acc2").children("a:first-child").removeClass("orangy");
+            jQuery(this).closest(".acc2").children("a:first-child").removeClass("orangy");
 
 
 
@@ -193,12 +192,12 @@ jQuery(".mask").mask("(999) 999-9999");
 
     });
 
-// Check to see if forms are complete on page load
-    jQuery(".acc2").each(function() {
+    // Check to see if forms are complete on page load
+    jQuery(".acc2").each(function () {
 
 
         // If all good then give the complete
-        if ((jQuery(this).find(".publisherName").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".webUrl").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".facebookLink").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".instagramName").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).closest(".acc2").find(".followed  input[type='checkbox']").is(":checked")) ) {
+        if ((jQuery(this).find(".publisherName").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".webUrl").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".facebookLink").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).find(".instagramName").val().replace(/^\s+|\s+$/g, "").length != 0) && (jQuery(this).closest(".acc2").find(".followed  input[type='checkbox']").is(":checked"))) {
 
             // set name
             var currentText = jQuery(this).find(".publisherName").val("blah");
@@ -225,7 +224,7 @@ jQuery(".mask").mask("(999) 999-9999");
     // Update progress bar on page load
     var chanItems = jQuery('.accordion.chan li').length;
     var chanComp = jQuery('.accordion.chan .check.showit').length;
-    var chanMultNum = 100/chanItems;
+    var chanMultNum = 100 / chanItems;
     var totalchandone = chanMultNum * chanComp;
     totalchandone = Math.round(totalchandone);
 
@@ -235,21 +234,21 @@ jQuery(".mask").mask("(999) 999-9999");
 
     if (chanComp < chanItems) {
         jQuery(".accordion.chan .wpcf7-submit").attr('disabled', 'disabled');
-    }else{
+    } else {
         jQuery(".accordion.chan a.decoy").addClass('off');
         jQuery(".accordion.chan .wpcf7-submit").removeClass('off');
     }
 
-    if(totalchandone <= 25){
+    if (totalchandone <= 25) {
         jQuery(".filled2").addClass('half');
-    } 
+    }
 
-    if(totalchandone >= 50){
+    if (totalchandone >= 50) {
         jQuery(".filled2").addClass('quart');
-    } 
+    }
 
 
-    jQuery(".acc2 input").on('blur click', function(e) {
+    jQuery(".acc2 input").on('blur click', function (e) {
 
         var chanItems = jQuery('.accordion.chan li').length;
         var chanComp = jQuery('.accordion.chan .check.showit').length;
@@ -273,38 +272,38 @@ jQuery(".mask").mask("(999) 999-9999");
 
         }
 
-    if(totalchandone <= 25){
-        jQuery(".filled2").addClass('half');
-    } 
+        if (totalchandone <= 25) {
+            jQuery(".filled2").addClass('half');
+        }
 
-    if(totalchandone >= 50){
-        jQuery(".filled2").addClass('quart');
-    } 
+        if (totalchandone >= 50) {
+            jQuery(".filled2").addClass('quart');
+        }
 
 
     });
 
-// Update territory value
-    jQuery('select').on('change', function() {
+    // Update territory value
+    jQuery('select').on('change', function () {
         var terit = jQuery(this).val();
         jQuery(this).closest(".tabs-panel").find("input.territory").val(terit);
         jQuery(".subfor span").text(terit);
-        if(jQuery(this).val()){
+        if (jQuery(this).val()) {
             jQuery(this).closest(".tabs-panel").find("div.form").removeClass("hideForm");
-        } else{
+        } else {
             jQuery(this).closest(".tabs-panel").find("div.form").addClass("hideForm");
         }
     });
 
 
 
-    jQuery("select").each(function() {
-        if(jQuery(this).val()){
+    jQuery("select").each(function () {
+        if (jQuery(this).val()) {
             var terit = jQuery(this).val();
             jQuery(this).closest(".tabs-panel").find("div.form").removeClass("hideForm");
             jQuery(".subfor span").text(terit);
 
-        } else{
+        } else {
             jQuery(this).closest(".tabs-panel").find("div.form").addClass("hideForm");
         }
     });
